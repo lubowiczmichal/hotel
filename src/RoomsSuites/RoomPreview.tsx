360024d9ab7db459fb202c28d030add1f08269e6
lubowicz.michal <lubowicz.michal@gmail.com>
2022-07-12 12:26:31 +0200

    currency can be selected

--- src/RoomsSuites/RoomPreview.tsx
+++ src/RoomsSuites/RoomPreview.tsx
@@ -1,30 +1,11 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import "./RoomPreview.css";
 import { Room } from "./RoomsData";
-import Autocomplete from "@mui/material/Autocomplete";
-import TextField from "@mui/material/TextField";
-import { NBPResponse } from "../Models";
-
-function RoomPreview(props: { room: Room }) {
-  const [currencies, setCurrencies] = useState<string[]>([]);
-  async function getNBPRates() {
-    await fetch("https://api.nbp.pl/api/exchangerates/tables/C/?format=json")
-      .then((res) => res.json())
-      .then(
-        (result: NBPResponse) => {
-          setCurrencies(
-            result.rates.map((item) => {
-              return item.currency;
-            })
-          );
-        },
-        (error) => console.log(error)
-      );
-  }
-  useEffect(() => {
-    getNBPRates();
-    console.log(currencies);
-  });
+function RoomPreview(props: {
+  room: Room;
+  currency: string;
+  currencyRate: number;
+}) {
   return (
     <div className="RoomPreview">
       <img id="Image" src={props.room.image} />
@@ -35,14 +16,10 @@ function RoomPreview(props: { room: Room }) {
         <li>{props.room.people}</li>
       </ul>
       <div className="PriceSection">
-        <p>{props.room.price}</p>
-        <Autocomplete
-          disablePortal
-          id="combo-box-demo"
-          options={currencies}
-          sx={{ width: 300 }}
-          renderInput={(params) => <TextField {...params} label="Movie" />}
-        />
+        <p>
+          {((props.room.price * 1) / props.currencyRate).toFixed(2)}{" "}
+          {props.currency}
+        </p>
       </div>
     </div>
   );
--- src/RoomsSuites/RoomsOverview.tsx
+++ src/RoomsSuites/RoomsOverview.tsx
@@ -1,18 +1,72 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./RoomsOverview.css";
 import NavBar from "../NavBar";
 import Footer from "../Footer";
 import TextWithBackground from "../TextWithBackground";
 import { roomData } from "./RoomsData";
 import RoomPreview from "./RoomPreview";
+import { NBPResponse, Rate } from "../Models";
+
+import Autocomplete from "@mui/material/Autocomplete";
+import TextField from "@mui/material/TextField";
 
 function RoomsOverview() {
   const rooms = roomData;
+  const [currencies, setCurrencies] = useState<Rate[]>([]);
+  const [selectedCurrency, setSelectedCurrency] = React.useState<string>("PLN");
+  React.useState<string>("");
+
+  const PLN: Rate = { code: "PLN", currency: "Zloty", ask: 1.0, bid: 1.0 };
+
+  async function getNBPRates() {
+    await fetch("https://api.nbp.pl/api/exchangerates/tables/C/?format=json")
+      .then((res) => res.json())
+      .then(
+        (result: NBPResponse[]) => {
+          const temp: Rate[] = result[0].rates.filter(
+            (rate) => rate.code != "XDR"
+          );
+          temp.push(PLN);
+          setCurrencies(temp);
+        },
+        (error) => console.log(error)
+      );
+  }
+
+  function getCurrencyRate(currencyCode: string) {
+    return currencies.filter((rate: Rate) => rate.code == currencyCode)[0].ask;
+  }
+
+  useEffect(() => {
+    getNBPRates();
+  }, []);
+
   return (
     <div className="RoomsOverview">
       <NavBar />
+      <Autocomplete
+        disablePortal
+        value={selectedCurrency}
+        onChange={(_: any, selected: string | null) => {
+          if (selected) {
+            setSelectedCurrency(selected);
+          }
+        }}
+        id="currency"
+        options={currencies
+          .map((currency) => {
+            return currency.code;
+          })
+          .sort()}
+        sx={{ width: 300 }}
+        renderInput={(params) => <TextField {...params} label="Currency" />}
+      />
       <TextWithBackground mode="rooms" />
-      <RoomPreview room={rooms[0]} />
+      <RoomPreview
+        room={rooms[0]}
+        currency={selectedCurrency}
+        currencyRate={getCurrencyRate(selectedCurrency)}
+      />
       <Footer />
     </div>
   );
